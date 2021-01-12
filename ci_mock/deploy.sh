ac4c1434673b10055ee29e10a35dfda9e2137fe5
Krzysztof Kakol (PGS Software) <kkakol@pgs-soft.com>
2021-01-12 19:59:13 +0100

    Fix

--- ci_mock/deploy.sh
+++ ci_mock/deploy.sh
@@ -2,12 +2,12 @@
 
 repository="kkakol/demoapp"
 branch="master"
-version="${1:-1.0.0}"
+version="${1:-}"
 
 commit=$(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1 | awk '{print tolower($0)}')
 
 if [ -z "${version}" ]; then
-    image="${repository}:${version}" #${branch}-${commit}"
+    image="${repository}:${branch}-${commit}"
 else
     image="${repository}:${version}"
 fi
